1f936122a137cf52a4c0852b162d9a5ca44f7a93
rbhatia <rbhatia@hmc.edu>
2023-05-22 19:49:38 -0700

    Minor changes

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 // This contains global variables to be accessed in the website!
-const INITIAL_VALUE_HEARTBEAT = 1000;
-const BAUD_RATE = 115200;
+const INITIAL_VALUE_HEARTBEAT = 1000; // Must be an integer.
+const BAUD_RATE = 115200; // Please have this match that on your Arduino device.
